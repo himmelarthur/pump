41e9ccb1d9307899a2b31a8182930e453ce7716a
Arthur Himmel <himmel.arthur@gmail.com>
2016-11-22 22:37:19 +0100

    Added logs

--- src/request/request.go
+++ src/request/request.go
@@ -60,13 +60,32 @@ func checkErr(err error) {
     }
 }
 
-func main() {
-    for page := 1; page <= 50; page++ {
-        get_track_page(page)
-    }
+func connect() (db *gorm.DB) {
+    DB_HOST, DB_USER, DB_PASSWORD, DB_NAME := os.Getenv("PUMP_DB_HOST"), os.Getenv("PUMP_DB_USER"), os.Getenv("PUMP_DB_PASSWORD"), os.Getenv("PUMP_DB_NAME")
+    db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
+    checkErr(err)
+    return
 }
 
-func save_tracks(db *gorm.DB, tracks []ResponseTrack) {
+func main() {
+    var db *gorm.DB = connect()
+
+    db.AutoMigrate(&Track{})
+
+    for page := 1; page <= 50; page++ {
+        fmt.Println("Fetching page", page)
+        var tracks []ResponseTrack = get_track_page(page)
+        fmt.Println("Saving tracks...", len(tracks))
+        fmt.Println("First track", tracks[0].Title)
+        err := save_tracks(db, tracks)
+        checkErr(err)
+        fmt.Println("Tracks saved!")
+    }
+    db.Close()
+}
+
+func save_tracks(db *gorm.DB, tracks []ResponseTrack) error{
+    tx := db.Begin()
     var db_track Track
     for _, track := range tracks {
         timestamp, _ := strconv.ParseInt(track.Date.Timestamp, 10, 64)
@@ -77,9 +96,13 @@ func save_tracks(db *gorm.DB, tracks []ResponseTrack) {
             Album: track.Album.Title,
             ListenedAt: track_date,
         }
-        db.Create(&db_track)
-        db.Save(&db_track)
+        if err := tx.Create(&db_track).Error; err != nil {
+            tx.Rollback()
+            return err
+        }
     }
+    tx.Commit()
+    return nil
 }
 
 func parse_tracks(body []byte) (*RecentTracksResponse, error) {
@@ -89,14 +112,7 @@ func parse_tracks(body []byte) (*RecentTracksResponse, error) {
     return response, err
 }
 
-func connect() (db *gorm.DB) {
-    DB_HOST, DB_USER, DB_PASSWORD, DB_NAME := os.Getenv("PUMP_DB_HOST"), os.Getenv("PUMP_DB_USER"), os.Getenv("PUMP_DB_PASSWORD"), os.Getenv("PUMP_DB_NAME")
-    db, err := gorm.Open("postgres", fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-    checkErr(err)
-    return
-}
-
-func get_track_page(page int) {
+func get_track_page(page int) []ResponseTrack{
     var API_KEY string = os.Getenv("LASTFM_API_KEY")
     var url string = fmt.Sprintf("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ArthurHimmel&api_key=%s&format=json&limit=10&page=%d", API_KEY, page)
     res, err := http.Get(url)
@@ -105,10 +121,5 @@ func get_track_page(page int) {
     checkErr(err)
     tracks_response, err := parse_tracks([]byte(body))
     checkErr(err)
-
-    var db *gorm.DB = connect()
-    defer db.Close()
-
-    db.AutoMigrate(&Track{})
-    save_tracks(db, tracks_response.TrackList.Tracks)
+    return tracks_response.TrackList.Tracks
 }
